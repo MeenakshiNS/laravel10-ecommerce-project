6d331d0723429fc188f3c17a875a77d3ae1cd11d
Meenakshi <hiringhrbot39@gmail.com>
2024-04-28 18:47:52 +0530

    first

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+use App\Http\Controllers\User\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+// use App\Http\Controllers\UserController;
+ 
+// Route::get('/user/{id}', [UserController::class, 'show']);
+
+Route::get('/',[HomeController::class,'home']);
